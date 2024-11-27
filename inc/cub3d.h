de5160fcaac68018d743d99939033b4fb39de1b1
Federico Carranza <fcarranz@student.42barcelona.com>
2024-11-27 16:38:29 +0100

    Refactor game parameter handling: add t_game struct, update set_game_params() to accept t_game, and implement parameter validation

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: davifer2 <davifer2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 20:21:49 by davifer2          #+#    #+#             */
-/*   Updated: 2024/11/26 13:16:31 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/11/27 16:33:19 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,41 @@
 # include <sys/time.h>
 # include "mlx.h"
 
+typedef struct	s_coords
+{
+	char	*NO;
+	char	*SO;
+	char	*WE;
+	char	*EA;
+}				t_coords;
 
+typedef struct	s_color
+{
+	char	*rgb;
+	int		red;
+	int		green;
+	int		blue;
+}				t_color;
+
+typedef struct	s_game
+{
+	t_color		ceiling;
+	t_color		floor;
+	t_coords	textures;
+	char		*map;
+}				t_game;
+
+// print_err.c
 int		print_err(const char *str, int ret);
+
+// map_validations.c
 int		ft_check_extension(const char *filename, const char *ext);
+
+// exit.c
 int		exit_err(const char *error, int ret);
-void	set_game_params(char *filename);
+void	free_t_game_params(t_game *cub3d);
+
+// set_params.c
+void	set_game_params(char *filename, t_game *cub3d);
 
 #endif
--- src/set_params.c
+++ src/set_params.c
@@ -3,36 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   set_params.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcarranz <fcarranz@student.42barcelon      +#+  +:+       +#+        */
+/*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:56:05 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/11/26 13:49:13 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/11/27 16:36:39 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdbool.h>
 #include "cub3d.h"
 #include "ft_printf.h"
 #include "get_next_line.h"
 
-static void	check_game_file(char *filename)
+static void	check_game_file(char *filename, int *fd)
 {
 	if (ft_check_extension(filename, ".cub"))
 		exit_err("Invalid extension", 1);
 	if (access(filename, F_OK | R_OK))
 		exit_err(strerror(errno), 1);
+	*fd  = open(filename, O_RDONLY);
+	if (*fd == -1)
+		exit_err(strerror(errno), 2);
 }
 
-void	set_game_params(char *filename)
+static bool	uncomplete_params(t_game *cub3d)
+{
+	t_coords	*textures;
+
+	textures = &cub3d->textures;
+	if (!cub3d->map || !cub3d->ceiling.rgb || !cub3d->floor.rgb)
+		return (true);
+	if (!textures->NO || !textures->SO || !textures->WE || !textures->EA)
+		return (true);
+	return (false);
+}
+
+void	set_game_params(char *filename, t_game *cub3d)
 {
 	int		fd;
 	char	*line;
 
-	check_game_file(filename);
-	fd  = open(filename, O_RDONLY);
-	if (fd == -1)
-		exit_err(strerror(errno), 2);
+	check_game_file(filename, &fd);
 	line = get_next_line(fd);
-	ft_printf("%s\n", line);
-	free(line);
+	while (line)
+	{
+		if (extract_params(&line, cub3d) == -1)
+			break ;	// out if error of any kind
+		line = get_next_line(fd);
+	}
 	close(fd);
+	if (uncomplete_params(cub3d))
+	{
+		free_t_game_ptrs(cub3d);
+		exit(EXIT_FAILURE);
+	}
 }
