65547e9ba8271a2eaf358898f7a9eaa54517b3e5
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-11-28 16:26:29 +0100

    Refactor and FIX BUGs on extract_coord() and set_coord()

--- src/extract_coord.c
+++ src/extract_coord.c
@@ -6,51 +6,64 @@
 /*   By: fcarranz <fcarranz@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 13:24:18 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/11/28 13:52:21 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/11/28 16:23:46 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "cub3d.h"
+#include "libft.h"
+
+static bool	set(char **param, char **value)
+{
+	if (*param)
+	{
+		print_err("To many declarations for the same coordinates", -1);
+		free (*value);
+		*value = NULL;
+		return (false);
+	}
+	*param = *value;
+	return (true);
+}
 
 static bool	set_coord(const char *tmp, const char *line, t_coords *textures)
 {
 	char	*value;
 
 	value = ft_rmspaces(line);
-	if (!ft_strncmp(tmp, "NO", 3) && !textures->NO)
-		textures->NO = value;
-	else if (!ft_strncmp(tmp, "SO", 3) && !textures->SO)
-		textures->SO = value;
-	else if (!ft_strncmp(tmp, "WE", 3) && !textures->WE)
-		textures->WE = value;
-	else if (!ft_strncmp(tmp, "EA", 3) && !textures->EA)
-		textures->EA = value;
-	return (true);
+	if (!value)
+		return (false);
+	if (!ft_strncmp(tmp, "NO", 3))
+		return (set(&textures->NO, &value));
+	else if (!ft_strncmp(tmp, "SO", 3))
+		return (set(&textures->SO, &value));
+	else if (!ft_strncmp(tmp, "WE", 3))
+		return (set(&textures->WE, &value));
+	else if (!ft_strncmp(tmp, "EA", 3))
+		return (set(&textures->EA, &value));
+	free (value);
+	return (false);
 }
 
 int	extract_coord(char *line, t_game *cub3d)
 {
 	char		*tmp;
 	bool		success;
-	const char	**coords = {"NO", "SO", "WE", "EA", NULL};
+	const char	*coords[] = {"NO", "SO", "WE", "EA", NULL};
 
-	tmp = (char *)ft_calloc(sizeof(char) * 3);
+	tmp = (char *)ft_calloc(3, sizeof(char));
 	if (!tmp)
-	{
-		print_err("Malloc failed!");
-		return (-1);
-	}
+		return (print_err("Malloc failed!", -1));
 	success = false;
 	tmp[0] = *line++;
-	while (is_space(*line))
+	while (ft_isspace(*line))
 		++line;
 	tmp[1] = *line++;
-	if (is_space(*line) && ft_strnarrcmp(tmp, coords, 3))
+	if (ft_isspace(*line) && ft_strnarrcmp(tmp, coords, 3))
 		success = set_coord(tmp, line, &cub3d->textures);
 	free (tmp);
 	if (success)
 		return (0);
-	print_err("Instruction not allowed in map file");
-	return (-1);
+	return (print_err("Instruction not allowed in map file", -1));
 }
