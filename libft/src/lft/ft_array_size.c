f917cab21bafa70b59d0a8d3aa15bfca49b1444c
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-12-10 13:26:27 +0100

    FIX BUG on ft_array_size()

--- libft/src/lft/ft_array_size.c
+++ libft/src/lft/ft_array_size.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 12:05:25 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/12/10 12:07:33 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/12/10 13:13:30 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_array_size(const char **array)
 	size_t	size;
 
 	size = 0;
-	while (array++)
+	while (array && *array++)
 		++size;
 	return (size);
 }
