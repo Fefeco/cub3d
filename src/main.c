a1bd3c9cd19796ed7cebde47f1b63613e600be7b
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-12-10 13:28:02 +0100

    Init colors

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:57:47 by davifer2          #+#    #+#             */
-/*   Updated: 2024/12/09 13:21:03 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/12/10 13:21:11 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@ void	init_t_game_ptrs(t_game *cub3d)
 	cub3d->textures.SO = NULL;
 	cub3d->textures.WE = NULL;
 	cub3d->textures.EA = NULL;
+	cub3d->ceiling.red = -1;
+	cub3d->ceiling.green = -1;
+	cub3d->ceiling.blue = -1;
+	cub3d->floor.red = -1;
+	cub3d->floor.green = -1;
+	cub3d->floor.blue = -1;
 	cub3d->ready_for_map = false;
 	cub3d->map = NULL;
 }
@@ -31,5 +37,6 @@ int	main(int argc, char **argv)
 	init_t_game_ptrs(&cub3d);
 	set_game_params(argv[1], &cub3d);	// Load params from .cub file
 	// init_game();						// Game initialization
+	free_t_game_ptrs(&cub3d);
 	return (0);
 }
