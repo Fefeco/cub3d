77247a62f0aed4e83e1fcf7d10d3b2f9f8f2c66d
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-12-11 13:08:18 +0100

    Refactor set_params()

--- src/set_params.c
+++ src/set_params.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:56:05 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/12/10 13:30:59 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/12/11 13:07:30 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,22 @@
 #include "ft_printf.h"
 #include "get_next_line.h"
 
-static void	check_game_file(char *filename, int *fd)
+static void	check_file_extension(char *filename)
 {
 	if (ft_check_extension(filename, ".cub"))
 		exit_err("Invalid extension", 1);
+}
+
+static int	open_source_file(char *filename)
+{
+	int	fd;
+
 	if (access(filename, F_OK | R_OK))
 		exit_err(strerror(errno), 1);
-	*fd = open(filename, O_RDONLY);
-	if (*fd == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		exit_err(strerror(errno), 2);
+	return (fd);
 }
 
 static bool	uncomplete_params(t_game *cub3d)
@@ -34,12 +41,12 @@ static bool	uncomplete_params(t_game *cub3d)
 	return (true);
 }
 
-void	set_game_params(char *filename, t_game *cub3d)
+static void	read_source_file(char *filename, t_game *cub3d)
 {
-	int		fd;
 	char	*line;
+	int		fd;
 
-	check_game_file(filename, &fd);
+	fd = open_source_file(filename);
 	line = get_next_line(fd);
 	while (line)
 	{
@@ -49,9 +56,17 @@ void	set_game_params(char *filename, t_game *cub3d)
 	}
 	close(fd);
 	test(cub3d);	// Prints game data;
-	if (uncomplete_params(cub3d)/* || check_map(cub3d->map) == -1*/)
+	if (uncomplete_params(cub3d))
 	{
 		free_t_game_ptrs(cub3d);
 		exit(EXIT_FAILURE);
 	}
+}
+
+void	set_game_params(char *filename, t_game *cub3d)
+{
+	check_file_extension(filename);
+	read_source_file(filename, cub3d);
+	//set_player_orient(cub3d);
+	//validate_map(cub3d->map);
 }
