bd27382496523e667112b32678156563a14c8228
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-12-12 12:38:27 +0100

    Add a function to free the principals structures of mlx and implement a working window

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: fedeito <fcarranz@student.42barcel>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 21:50:22 by fedeito           #+#    #+#             */
-/*   Updated: 2024/12/11 22:19:11 by fedeito          ###   ########.fr       */
+/*   Updated: 2024/12/12 12:33:54 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,33 @@
 #include "ft_printf.h"
 #include "colors.h"
 
+static void	mlx_clean_exit(t_mlx *mlx)
+{
+	if (mlx->win)
+	{
+		mlx_destroy_window(mlx->disp, mlx->win);
+		free (mlx->win);
+		mlx->win = NULL;
+	}
+	if (mlx->disp)
+	{
+		mlx_destroy_display(mlx->disp);
+		free (mlx->disp);
+		mlx->disp = NULL;
+	}
+}
+
 void	init_game(t_game *cub3d)
 {
-	void	*connection;
-	void	*window;
+	t_mlx	*mlx;
 
-	(void) cub3d;
-	connection = mlx_init();
-	if (!connection)
+	mlx = &cub3d->mlx;
+	mlx->disp = mlx_init();
+	if (!mlx->disp)
 		return ;
 	ft_printf(YELL"Connection stablished!\n"RESET);
-	window = mlx_new_window(connection, WIDTH, HEIGTH, "CUB3D");
-	mlx_loop(connection);
+	mlx->win = mlx_new_window(mlx->disp, WIDTH, HEIGTH, "CUB3D");
+	mlx_loop(mlx->disp);
 
-	mlx_destroy_window(connection, window);
-	mlx_destroy_display(connection);
-	free (connection);
+	mlx_clean_exit(mlx);
 }
