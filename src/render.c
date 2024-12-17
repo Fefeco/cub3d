058cff392677b2538c946824baf889746eab2d33
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-12-17 11:35:20 +0100

    fix duplicated func

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 20:45:33 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/12/16 14:21:07 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/12/17 11:33:53 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,7 @@ static void	put_pxl_on_img(t_img *images, int x, int y, int color)
 			+ x * (images->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
-static void	put_pxl_on_img(t_img *images, int x, int y, int color)
-{
-	char	*dst;
 
-	dst = images->addr_to_draw + (y * images->line_length
-			+ x * (images->bits_per_pixel / 8));
-	dst = color;
-}
 static void	draw_background(t_game *cub3d)
 {
 	t_img	*images;
